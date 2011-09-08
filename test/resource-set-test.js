ce08b96370d32ee58ef8566fde6b1b6c37ad1888
August Lilleaas <august.lilleaas@shortcut.no>
2011-09-08 20:22:08 +0200

    Creating the resource sets through the buster-resources module, which is the only real supported way of creating them anyways.

--- test/resource-set-test.js
+++ test/resource-set-test.js
@@ -1,12 +1,16 @@
 var buster = require("buster");
 var assert = buster.assert;
-var resourceSet = require("./../lib/resource-set");
+var busterResources = require("./../lib/buster-resources");
 
 // For legacy reasons, most of the resource-set tests are encapsulated in session
 // and capture tests.
 buster.testCase("resource-set", {
+    setUp: function () {
+        this.br = Object.create(busterResources);
+    },
+
     "test creating with blank object": function () {
-        var r = resourceSet.create({});
+        var r = this.br.createResourceSet({});
         assert(r.load instanceof Array);
         assert.equals(r.load.length, 0);
 
@@ -14,7 +18,7 @@ buster.testCase("resource-set", {
     },
 
     "test setting context path after creation": function () {
-        var r = resourceSet.create({});
+        var r = this.br.createResourceSet({});
         r.contextPath = "/foo";
 
         assert.equals("/foo", r.contextPath);
