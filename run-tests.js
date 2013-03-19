66c0faaf1f42f774cc4b3eb70b1b1301505a06d8
Christian Johansen <christian@cjohansen.no>
2013-03-19 23:49:25 +0100

    Fix test runs on Node 0.10.0

---
+++ run-tests.js
@@ -0,0 +1,19 @@
+var buster = require("buster");
+
+buster.testRunner.onCreate(function (runner) {
+    runner.on("suite:end", function (results) {
+        if (!results.ok) {
+            setTimeout(function () {
+                process.exit(1);
+            }, 50);
+        }
+    });
+});
+
+require("./test/http-proxy-test");
+require("./test/load-path-test");
+require("./test/processors/iife-processor-test");
+require("./test/resource-middleware-test");
+require("./test/resource-set-cache-test");
+require("./test/resource-set-test");
+require("./test/resource-test");
